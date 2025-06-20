3e5447c86a7bd42794da3091cb4c5e922e4d7920
Jackson <jackconnerdoyle@gmail.com>
2025-06-19 22:09:55 -0700

    Added show/hide password buttons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,6 +7,7 @@ function Login() {
   // If 200, redirect to Home page
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPasswords, setShowPasswords] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
@@ -73,14 +74,59 @@ function Login() {
           <label className="block mb-1 text-sm font-medium text-gray-700">
             Password
           </label>
-          <input
-            type="password"
-            name="password"
-            className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <div className="relative">
+            <input
+              type={showPasswords ? "text" : "password"}
+              name="password"
+              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <div
+              className="absolute inset-y-0 right-2 flex items-center cursor-pointer text-gray-500"
+              onClick={() => setShowPasswords((prev) => !prev)}
+            >
+              {showPasswords ? (
+                // Open eye
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              ) : (
+                // Closed eye
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              )}
+            </div>
+          </div>
         </div>
         <button
           type="submit"
@@ -89,6 +135,7 @@ function Login() {
           Sign In
         </button>
         <button
+          type="button"
           onClick={() => navigate("/register")}
           className="w-full mt-4 px-6 py-2 bg-black text-white rounded-md opacity-70 transition-colors cursor-pointer"
         >
